8f436bd97b84337a6102e0eb79bc578c92e4052a
maxts0gt <bazarsadtsogtjargal@gmail.com>
2021-09-30 00:41:07 +0900

    Installed and sat up session

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/alexedwards/scs/v2"
 	"log"
 	"net/http"
+	"time"
 	"web-dev-go/pkg/config"
 	"web-dev-go/pkg/handlers"
 	"web-dev-go/pkg/render"
 )
 
- const portNumber = ":8080"
-
-
+const portNumber = ":8080"
+var app config.AppConfig
+var session *scs.SessionManager
  // main is the main application function
 func main() {
-	var app config.AppConfig
+
+
+	app.InProduction = false
+	// in production, change it to true
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false //in development mode it should be false. since we are hitting localhost:8080
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
